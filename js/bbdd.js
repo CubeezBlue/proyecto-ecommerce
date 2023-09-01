367761f15f52b2c6a4a2de1ae0c9cf40a17b4b3b
Nicolas Peralta <nicomartin98@hotmail.com>
2023-08-31 22:32:41 -0300

    new proyect

---
+++ js/bbdd.js
@@ -0,0 +1,111 @@
+export const usuarios = [{
+        usuario: "nico98",
+        contraseña: "n1234"
+    },
+    {
+        usuario: "jorge24",
+        contraseña: "j1234"
+    },
+    {
+        usuario: "tito90",
+        contraseña: "t1234"
+    },
+    {
+        usuario: "fede77",
+        contraseña: "f1234"
+    },
+    {
+        usuario: "leti12",
+        contraseña: "l1234"
+    }
+]
+
+export const productos = [{
+        id: 1,
+        nombre: "Buzo Nike Edition Originals",
+        descripcion: "Buzo Nike negro, con pipeta roja. Material de algodon, super abrigado.",
+        precio: 26750,
+        precioTotal: 1,
+        img: "../img/buzo-nike.webp",
+        cantidad: 1
+    },
+    {
+        id: 2,
+        nombre: "Remera Adidas Originals",
+        descripcion: "Remera Adidas edicion orginals, con estampado de flores, color blanca, pipeta negra",
+        precio: 13200,
+        precioTotal: 1,
+        img: "../img/remera-adidas.jpeg",
+        cantidad: 1
+    },
+    {
+        id: 3,
+        nombre: "Remera Jorda",
+        descripcion: "Remera Nike Jordan, color negra con el logo de las zapatillas",
+        precio: 11430,
+        precioTotal: 1,
+        img: "../img/remera-jordan.jpeg",
+        cantidad: 1
+    },
+    {
+        id: 4,
+        nombre: "Remera Nike Oversize",
+        descripcion: "Remera Nike estilo oversize urbana, color azul oscuro",
+        precio: 14300,
+        precioTotal: 1,
+        img: "../img/remera-nike-oversize.jpg",
+        cantidad: 1
+    },
+    {
+        id: 5,
+        nombre: "Buzo Adidas Originals",
+        descripcion: "Buzo Adidas Rojo, version originals, clasico para todo tipo de evento",
+        precio: 21900,
+        precioTotal: 1,
+        img: "../img/buzo-adidas.jpeg",
+        cantidad: 1
+    },
+    {
+        id: 6,
+        nombre: "Pantalon Adidas Orginals",
+        descripcion: "Pantalon Adidas Original, tela depotiva. Perfecto para ejericio",
+        precio: 16450,
+        precioTotal: 1,
+        img: "../img/pantalon-adidas.jpeg",
+        cantidad: 1
+    },
+    {
+        id: 7,
+        nombre: "Pantalon Nike",
+        descripcion: "Pantalon Nike, tela de algodon.",
+        precio: 11340,
+        precioTotal: 1,
+        img: "../img/pantalon-nike.jpeg",
+        cantidad: 1
+    }, {
+        id: 8,
+        nombre: "Campera Adidas Retro",
+        descripcion: "Campera Adidas Originals, modelo retro color violeta",
+        precio: 12100,
+        img: "../img/campera-adidas.jpg",
+        cantidad: 1
+    }, {
+        id: 9,
+        nombre: "Campera Nike Vintage",
+        descripcion: "Campera Nike Retro/Vintage, edicion limitada",
+        precio: 17400,
+        precioTotal: 1,
+        img: "../img/campera-nike.webp",
+        cantidad: 1
+    },
+    {
+        id: 10,
+        nombre: "Zapatilla Adidas Orginals",
+        descripcion: "Zapatilla Adidas, version Orginals color mostaza, estilo retro.",
+        precio: 40700,
+        precioTotal: 1,
+        img: "../img/zapatilla-adidas.webp",
+        cantidad: 1
+    }
+
+]
---
+++ js/main.js
@@ -0,0 +1,65 @@
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const btnVerCarrito = document.querySelector("#carrito");
+const bgCarrito = document.querySelector(".bg-carrito-html");
+const carritoContenedor = document.querySelector("#contenedorCarrito");
+const totalCarrito = document.querySelector("#total-carrito");
+const btnCerrar = document.querySelector(".close");
+//Viendo los productos agregados al carrito
+
+// Creando y Agregando Productos al Carrito
+btnVerCarrito.addEventListener("click", () => {
+    bgCarrito.classList.remove("d-none");
+    carritoContenedor.innerHTML = "";
+    carrito.forEach(p => {
+        const divProducto = document.createElement("div");
+        divProducto.classList.add("producto-carrito");
+        divProducto.innerHTML = `
+        <img src="${p.img}" alt="">
+        <div class="desc-producto">
+            <p>${p.nombre}</p>
+            <P>3 cuotas sin interes</P>
+            <p>$${p.precioTotal}</p>
+            <p>Cantidad: ${p.cantidad}</p>
+        </div>
+        <div class="btn-productos" id="contenedor-${p.id}">
+            <a class="btn-eliminar"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /> 
+            </svg>
+            </a>
+        </div>
+        `
+
+
+        //Sumar Total de los productos
+        let totalCarritoPrecio = 0;
+        carrito.forEach(p => {
+            totalCarritoPrecio += p.precioTotal;
+            totalCarrito.innerHTML = `EL TOTAL DE SU COMPRA ES DE $${totalCarritoPrecio}`
+        })
+
+        // Boton Eliminar Producto del Carrito
+        const btnEliminar = divProducto.querySelector(".btn-eliminar");
+        btnEliminar.addEventListener("click", () => {
+            const index = carrito.indexOf(p);
+            if (index !== -1) {
+                carrito.splice(index, 1);
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                carritoContenedor.removeChild(divProducto);
+            }
+        });
+
+
+
+        carritoContenedor.appendChild(divProducto);
+
+    })
+
+
+})
+
+// Boton Cerrar Carrito
+
+btnCerrar.addEventListener("click", () => {
+    bgCarrito.classList.add("d-none");
+})
