d5ee3132af3c850480f107269a7b2c73be19a767
Nicolas Peralta <nicomartin98@hotmail.com>
2023-09-01 00:28:38 -0300

    rras

--- js/pages/productos.js
+++ js/pages/productos.js
@@ -26,7 +26,7 @@ productos.forEach(p => {
 
     contendorBtn.appendChild(btnCarrito);
 
-    btnCarrito.addEventListener("click", () => {
+    contendorBtn.addEventListener("click", () => {
         const existingProduct = carrito.find(producto => producto.id === p.id);
 
         if (existingProduct) {
@@ -39,6 +39,7 @@ productos.forEach(p => {
 
         localStorage.setItem("carrito", JSON.stringify(carrito));
 
+        actualizarVistaCarrito();
 
     });
 
@@ -91,34 +92,10 @@ btnVerCarrito.addEventListener("click", () => {
                     carrito.splice(index, 1);
                     localStorage.setItem("carrito", JSON.stringify(carrito));
                     carritoContenedor.removeChild(divProducto);
-                    let totalCarritoPrecio = 0;
-                    carrito.forEach(p => {
-                        carritoContenedor.innerHTML = "";
-                        carrito.forEach(p => {
-                            const divProducto = document.createElement("div");
-                            divProducto.classList.add("producto-carrito");
-                            divProducto.innerHTML = `
-        <img src="../${p.img}" alt="">
-        <div class="desc-producto">
-            <p>${p.nombre}</p>
-            <P>3 cuotas sin interes</P>
-            <p>$${p.precioTotal}</p>
-            <p>Cantidad: ${p.cantidad}</p>
-        </div>
-        <div class="btn-productos" id="contenedor-${p.id}">
-            <a class="btn-eliminar"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /> 
-            </svg>
-            </a>
-        </div>
-        `
-                        })
-                        totalCarritoPrecio += p.precioTotal;
-                        totalCarrito.innerHTML = `EL TOTAL DE SU COMPRA ES DE $${totalCarritoPrecio}`
-                    })
+                    actualizarVistaCarrito();
                 }
-            });
 
+            });
 
 
             carritoContenedor.appendChild(divProducto);
@@ -132,3 +109,46 @@ btnVerCarrito.addEventListener("click", () => {
 btnCerrar.addEventListener("click", () => {
     bgCarrito.classList.add("d-none");
 })
+
+// Actulizador de Carrito
+
+function actualizarVistaCarrito() {
+    carritoContenedor.innerHTML = "";
+    let totalCarritoPrecio = 0;
+
+    carrito.forEach(p => {
+        const divProducto = document.createElement("div");
+        divProducto.classList.add("producto-carrito");
+        divProducto.innerHTML = `
+            <img src="../${p.img}" alt="">
+            <div class="desc-producto">
+                <p>${p.nombre}</p>
+                <P>3 cuotas sin interes</P>
+                <p>$${p.precioTotal}</p>
+                <p>Cantidad: ${p.cantidad}</p>
+            </div>
+            <div class="btn-productos" id="contenedor-${p.id}">
+                <a class="btn-eliminar"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <!-- SVG path data -->
+                </svg></a>
+            </div>
+        `;
+
+        // Boton Eliminar Producto del Carrito
+        const btnEliminar = divProducto.querySelector(".btn-eliminar");
+        btnEliminar.addEventListener("click", () => {
+            const index = carrito.indexOf(p);
+            if (index !== -1) {
+                carrito.splice(index, 1);
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                carritoContenedor.removeChild(divProducto);
+            }
+
+        });
+
+        totalCarritoPrecio += p.precioTotal;
+        carritoContenedor.appendChild(divProducto);
+    });
+
+    totalCarrito.innerHTML = `EL TOTAL DE SU COMPRA ES DE $${totalCarritoPrecio.toFixed(2)}`;
+}
